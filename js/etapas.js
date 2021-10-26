911144cfeb5299c7b8bae337a7a5be6586ff6e7e
Vítor Souza <vitor.souza407@yahoo.com.br>
2021-10-26 20:21:12 -0300

    first commit

---
+++ js/etapas.js
@@ -0,0 +1,54 @@
+let etapas = [
+    {
+        titulo: 'Vereador',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                name: 'Fulano de Tal',
+                partido: 'BRS',
+                fotos: [
+                    {url: 'prefeito_1.jpg', legenda: 'Vereador', small:false}
+                ]
+            },
+            
+            {
+                numero: '77222',
+                name: 'Beltrano da Silva',
+                partido: 'BRTS',
+                fotos: [
+                    {url: 'prefeito_2.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+
+    },
+
+    {
+        titulo: 'Prefeiro',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                name: 'Ciclano',
+                partido: 'BRS',
+                vice: 'Cic',
+                fotos: [
+                         {url:'99.jpg', legenda:'Prefeito'},
+                         {url:'99_2.jpg', legenda:'Vice-Prefeito', small:true}
+                    ]
+            },
+                {
+                    numero: '84',
+                    name: 'Zulano',
+                    partido: 'BRTS',
+                    vice: 'Zul',
+                    fotos: [
+                      {url: '84.jpg', legenda:'Prefeito'},
+                      {url:'84_2.jpg', legenda:'Vice-Prefeito', small:true}
+                    ]
+                    
+              }
+        ]
+    }
+];
